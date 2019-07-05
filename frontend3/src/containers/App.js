fe22049e2f48cb769223060e423f8eb292096a18
oasis345 <oasis345>
2019-07-05 10:28:57 +0900

    first commit3

---
+++ frontend3/src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/container/organisms/Navbar';
+import { Route, Redirect } from 'react-router'
+
+function App() {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+export default App;
